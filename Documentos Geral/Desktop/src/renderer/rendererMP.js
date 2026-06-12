c087a9669acf584d7935cf9d36d7f74296d13057
Eduardo-P-B <epiresbatagliotti@gmail.com>
2026-06-11 22:37:56 -0300

    ah mano ta ai
    arrumei umas coisas e fiz o forms (cansei tmnc demorou mais doq deveria essa porra) todas as aulas até a aula 12 estão feitas

--- Documentos Geral/Desktop/src/renderer/rendererMP.js
+++ Documentos Geral/Desktop/src/renderer/rendererMP.js
@@ -1,32 +1,89 @@
-const carregarTela = async () => {
-    // 1. Liga para o Node e ESPERA o JSON chegar
-    const listaPlantas = await window.api.pedirPlantas();
+let listaPlantas = [];
 
-    // 2. Mapeia os dados para gerar colunas (col) com cards do Bootstrap
-    const html = listaPlantas.map(plant => `
+const form = document.querySelector("#cadastro-planta-form");
 
-        <div class="plant-item">
-            <div class="plant-preview">
-                <img src="images/${plant.imagem}"">
-            </div>
-            <h4>${plant.nome}</h4>
-            <p>Nível de Umidade:</p>
-            <div class="progress-bar">
-                <div class="water-progress" style="width: ${plant.nivelUmidade}%"></div>
-            </div>
-            <p>Nível de luz Solar</p>
-            <div class="progress-bar">
-                <div class="sun-progress" style="width: ${plant.nivelLuz}%"></div>
-            </div>
-        </div>
-    `).join("");
+const desenharTela = (listaParaDesenhar) => {
 
-    // 3. Joga na tela dentro de um contêiner de linha (row)
+    const html = listaParaDesenhar.map(plant => {
+
+        let classeCard = "";
+
+        if (plant.nivelUmidade <= 20 || plant.nivelLuz <= 20) {
+            classeCard = "urgent";
+        }
+        else if (plant.nivelUmidade <= 40 || plant.nivelLuz <= 40) {
+            classeCard = "warning";
+        }
+
+        return `
+            <div class="plant-item ${classeCard}">
+                <div class="plant-preview">
+                    <img src="images/${plant.imagem}" alt="${plant.nome}">
+                </div>
+
+                <h4>${plant.nome}</h4>
+
+                <p>Nível de Umidade:</p>
+                <div class="progress-bar">
+                    <div class="water-progress" style="width:${plant.nivelUmidade}%"></div>
+                </div>
+
+                <p>Nível de luz Solar:</p>
+                <div class="progress-bar">
+                    <div class="sun-progress" style="width:${plant.nivelLuz}%"></div>
+                </div>
+
+                <p>Localização: ${plant.localizacao || "Não informada"}</p>
+            </div>
+        `;
+    }).join("");
+
     document.querySelector("#conteudo").innerHTML = html;
 };
 
-// 4. Executa a função assim que o script carregar!
-carregarTela();
+const iniciarApp = async () => {
+    listaPlantas = await window.api.pedirPlantas();
+    desenharTela(listaPlantas);
+};
+
+iniciarApp(); // Dá a partida no programa
+
+const barraBusca = document.querySelector("#inputBusca");
+
+barraBusca.addEventListener("input", () => {
+
+    const textoDigitado = barraBusca.value.toLowerCase();
+
+    const listaFiltrada = listaPlantas.filter(plant => {
+        return (
+            plant.nome.toLowerCase().includes(textoDigitado) ||
+            plant.localizacao.toLowerCase().includes(textoDigitado)
+        );
+    });
+
+    desenharTela(listaFiltrada);
+
+});
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const extrator = new FormData(form);
+    const novaPlanta = Object.fromEntries(extrator);
+
+    console.log(novaPlanta);
+
+    novaPlanta.nivelUmidade = 100;
+    novaPlanta.nivelLuz = 100;
+    novaPlanta.imagem = "plantaPadrao.png";
+
+    listaPlantas.push(novaPlanta);
+
+    desenharTela(listaPlantas);
+
+    form.reset();
+});
 
 const novaPlantaBtn = document.getElementById('nova-planta-btn');
 const heroCard = document.getElementById('hero-card');
